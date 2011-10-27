e56f0962bf32df9823ae0afb27332988baf20257
Marc <marc.dhaubenstock@gmail.com>
2011-10-27 18:10:25 +0100

    Apple OpenGL header detection

--- visigoth/glgraphwidget.cpp
+++ visigoth/glgraphwidget.cpp
@@ -1,7 +1,12 @@
 #include <QMouseEvent>
 #include <QKeyEvent>
 #include <cmath>
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else
 #include <GL/gl.h>
+#endif
 
 #include "glgraphwidget.h"
 #include "glancillary.h"        // gla*()
